686c9a5fca60220a0815bd4a437f7eee89bad7ab
Rowland2023 <rowlandobi@gmail.com>
2025-06-25 16:19:20 +0100

    Authentication,register,login,profile

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -6,6 +6,11 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+        #read_only_fields = ['author']
+    #def create(self, validated_data):
+    #    validated_data['author'] = self.context['request'].user
+    #    return super().create(validated_data)
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,8 +1,16 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from.views import PostViewSets ,CommentViewSets
+from.views import PostViewSet ,CommentViewSet,register,login,profile,post_comments
 
 router = DefaultRouter()
-router.register(r'post',PostViewSets)
-router.register(r'comments',CommentViewSets)
+router.register(r'posts',PostViewSet)
+router.register(r'comments',CommentViewSet)
 
-urlpatterns = router.urls
+urlpatterns =[
+    path('register/' ,register),
+    path('login/',login) , 
+    path('profile/',profile),
+    path('posts/<int:post_id>/comments/', post_comments, name='post-comments'),
+]
+
+urlpatterns += router.urls
